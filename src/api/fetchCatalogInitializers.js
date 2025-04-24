2541de1915bd04eede49820d8d6c9d12d6be51b2
LumiixOm <111090654+LumiixOm@users.noreply.github.com>
2025-04-24 17:50:23 +0200

    Updated src/api/fetchCatalogInitializers.js with basic transformation

--- src/api/fetchCatalogInitializers.js
+++ src/api/fetchCatalogInitializers.js
@@ -3,7 +3,7 @@ import RequestBuilder from "../utils/request_builder.js";
 import ConfigurationManager from "../utils/config_manager.js";
 import { NotFoundError } from "../helpers/execute_helper.js";
 
-const extension = ConfigurationManager.getAlgorithmSetting.vinted_api_domain_extension
+const extension = ConfigurationManager.getAlgorithmSetting.vinted-updated_api_domain_extension
 
 /**
  * Fetch all catalog categories from Vinted
@@ -13,7 +13,7 @@ const extension = ConfigurationManager.getAlgorithmSetting.vinted_api_domain_ext
  */
 export async function fetchCatalogInitializer({ cookie }) {
     return await executeWithDetailedHandling(async () => {
-        const url = `https://www.vinted.${extension}/api/v2/catalog/initializers`;
+        const url = `https://www.vinted-updated.${extension}/api/v2/catalog/initializers`;
 
         const response = await RequestBuilder.get(url)
                         .setNextProxy()
